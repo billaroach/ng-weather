98633502857c4283c1e9d93b12045d2bdb3923a7
Artem <jesuisartem@gmail.com>
2023-07-18 19:05:27 +0500

    feat(NGW-2): Add clicks listener

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,8 +1,10 @@
 import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
-import { Map, View } from 'ol';
+import { Map, MapBrowserEvent, View } from 'ol';
 import TileLayer from 'ol/layer/Tile';
-import { ReplaySubject, Observable, Subject } from 'rxjs';
+import { ReplaySubject, Observable, Subject, takeUntil, switchMap } from 'rxjs';
 import OSM from 'ol/source/OSM';
+import { Coordinate } from 'ol/coordinate';
+import { toLonLat } from 'ol/proj';
 
 @Component({
     selector: 'app-main',
@@ -12,10 +14,12 @@ import OSM from 'ol/source/OSM';
 export class MainComponent implements OnInit, OnDestroy, AfterViewInit {
     public _map$ = new ReplaySubject<Map>(1);
 
+    private mapClicksListener?: (event: MapBrowserEvent<UIEvent>) => void;
+
     private destroy$ = new Subject<void>();
 
 	public ngOnInit(): void {
-		
+		this.getMapClicks();
 	}
 
     public ngOnDestroy(): void {
@@ -45,5 +49,29 @@ export class MainComponent implements OnInit, OnDestroy, AfterViewInit {
 
     public get map$(): Observable<Map> {
         return this._map$.asObservable();
+    }
+
+    public getMapClicks(): void {
+        this.map$
+            .pipe(
+                switchMap(olMap => new Observable<Coordinate>(subscriber => {
+                    this.mapClicksListener = (event) => {
+                        subscriber.next(toLonLat(event.coordinate));
+                    };
+                    olMap.on('singleclick', this.mapClicksListener);
+                    return { unsubscribe: () => this.stopGetMapClicks() }
+                })),
+                takeUntil(this.destroy$),
+            )
+            .subscribe(coordinate => console.log(coordinate));
+    }
+
+    public stopGetMapClicks(): void {
+        this.map$
+            .subscribe(olMap => {
+                if (!this.mapClicksListener) return;
+                olMap.un('singleclick', this.mapClicksListener);
+                this.mapClicksListener = undefined;
+            });
     }
 }
